768d3e010ae748283dc855a4f8932de0e0ba9b4e
Karen Pearl <kpearl099@gmail.com>
2026-08-25 10:13:21 -0700

    fix imports

--- src/lib/editor.js
+++ src/lib/editor.js
@@ -2,10 +2,7 @@ import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
 import TaskList from '@tiptap/extension-task-list';
 import TaskItem from '@tiptap/extension-task-item';
-import Table from '@tiptap/extension-table';
-import TableRow from '@tiptap/extension-table-row';
-import TableHeader from '@tiptap/extension-table-header';
-import TableCell from '@tiptap/extension-table-cell';
+import { Table, TableRow, TableHeader, TableCell } from '@tiptap/extension-table';
 import Link from '@tiptap/extension-link';
 import Underline from '@tiptap/extension-underline';
 
